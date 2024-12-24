dacbf358aacd0c583ddeba7538f9c973c2ea40fc
Antonio <antonio.adrian@maibornwolff.de>
2024-12-24 11:55:14 +0100

    feat: only lower case

--- insider/game/models.py
+++ insider/game/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-
 class Word(models.Model):
     word = models.CharField(max_length=64, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.word = str(self.word).lower()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.word)
