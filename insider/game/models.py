7e0682c8f2f7a3a89f191ab031a21ba4199369f6
Antonio <antonio.adrian@maibornwolff.de>
2024-12-24 11:08:33 +0100

    Add word model

--- insider/game/models.py
+++ insider/game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Words(models.Model):
+    word = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return str(self.word)
